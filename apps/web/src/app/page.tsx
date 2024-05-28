92c9aed45f7a45685e16a53adcca2bd595978c7f
Himanshu-AT <aristech.official@gmail.com>
2024-05-28 12:03:59 +0530

    complete home Page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,113 +1,214 @@
+import { FlipWords } from "@/components/animations/flipWord";
+import { Boxes } from "@/components/animations/hero/grid-anim";
+import { InfiniteMovingCards } from "@/components/animations/infiniteMovingcard";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { CheckSquareIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <>
+      <div className=" min-h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg mb-5">
+        <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+        <Boxes />
+        <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
+          Introducing Declutter
+        </h1>
+        <p className="text-center text-primary-foreground mt-2 text-neutral-300 relative z-20">
+          xplore a revolutionary way to organize your mind with concepts like
+          <FlipWords
+            className="text-primary-foreground text-white"
+            words={["Kanban", "Mind Mapping", "Pomodoro"]}
+          />
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+      </div>
+      <section>
+        <h1 className="text-center text-4xl text-white mb-4">Features</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="p-4 bg-slate-800 rounded-lg">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/kanban.svg"
+              alt="Kanban Board image"
+              height={64}
+              width={64}
             />
-          </a>
+            <h2 className="text-white text-xl mt-2">Kanban Boards</h2>
+            <p className="text-neutral-300 mt-2">
+              Organize your tasks in a visual way with Kanban boards.
+            </p>
+          </div>
+          <div className="p-4 bg-slate-800 rounded-lg">
+            <Image
+              src="/mindmap.svg"
+              alt="Mind Map board image"
+              height={64}
+              width={64}
+            />
+            <h2 className="text-white text-xl mt-2">Mind Mapping</h2>
+            <p className="text-neutral-300 mt-2">
+              Visualize your thoughts and ideas with mind maps.
+            </p>
+          </div>
+          <div className="p-4 bg-slate-800 rounded-lg">
+            <Image
+              src="/pomodoro.svg"
+              alt="Pomodoro Image"
+              height={64}
+              width={64}
+            />
+            <h2 className="text-white text-xl mt-2">Pomodoro Timer</h2>
+            <p className="text-neutral-300 mt-2">
+              Stay focused and productive with the Pomodoro technique.
+            </p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </section>
+      <section>
+        <h1 className="text-center text-4xl text-white mb-4 mt-9">Pricing</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <PricingCard
+            title="Free"
+            description="Get started for free."
+            price={0}
+            currency="USD"
+            features={["Limited boards", "Limited tasks", "Limited users"]}
+          />
+          <PricingCard
+            title="Pro"
+            description="Unlock all features."
+            price={10}
+            currency="USD"
+            features={[
+              "Unlimited boards",
+              "Unlimited tasks",
+              "Unlimited users",
+            ]}
+          />
+          <PricingCard
+            title="Enterprise"
+            description="Customize your experience."
+            priceText={{
+              Text: "Get in touch",
+              link: "/contact",
+            }}
+            features={[
+              "Custom branding",
+              "Custom integrations",
+              "Dedicated support",
+            ]}
+          />
+        </div>
+      </section>
+      <section className=" mt-9 mb-4">
+        <h1 className="text-center text-4xl text-white mb-4">Testimonials</h1>
+        <TestimonialCards />
+      </section>
+    </>
   );
 }
+
+const PricingCard = ({
+  title,
+  description,
+  price,
+  currency,
+  priceText,
+  features,
+}: {
+  title: string;
+  description: string;
+  price?: number;
+  priceText?: { Text: string; link: string };
+  currency?: string;
+  features: string[];
+}) => {
+  return (
+    <>
+      <div className="p-4 bg-slate-800 rounded-lg">
+        <h2 className="text-white text-2xl mt-2 text-center">{title}</h2>
+        <p className=" text-muted-foreground mt-2 text-center text-xl">
+          {description}
+        </p>
+        {features &&
+          features.map((feature) => (
+            <span
+              className="flex flex-row space-x-2 mt-2 space-y-1"
+              key={feature}
+            >
+              <CheckSquareIcon color=" green" size={20} />
+              <p className="text-neutral-300 ">{feature}</p>
+            </span>
+          ))}
+        {price !== undefined ||
+        currency != undefined ||
+        price != null ||
+        currency != null ? (
+          <Button
+            variant={"default"}
+            className=" rounded-full min-w-full text-lg my-5 hover:bg-slate-700"
+          >
+            {price} {currency}
+          </Button>
+        ) : null}
+
+        {priceText ? (
+          <Link href={priceText.link}>
+            <Button
+              variant={"default"}
+              className=" rounded-full min-w-full text-lg my-5 hover:bg-slate-700"
+            >
+              {priceText.Text}
+            </Button>
+          </Link>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+function TestimonialCards() {
+  return (
+    <div className=" rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
+      <InfiniteMovingCards
+        items={testimonials}
+        direction="right"
+        speed="slow"
+      />
+    </div>
+  );
+}
+
+const testimonials = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
